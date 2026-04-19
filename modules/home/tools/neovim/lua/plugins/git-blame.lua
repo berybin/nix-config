b119b3b149d810311b526fc2134e38f06cdad6d2
jay <me@jayparry.dev>
2026-04-19 17:29:56 +1000

    nvim: move git-blame to status line

--- modules/home/tools/neovim/lua/plugins/git-blame.lua
+++ modules/home/tools/neovim/lua/plugins/git-blame.lua
@@ -1,10 +1,26 @@
 -- https://github.com/f-person/git-blame.nvim
 return {
-	"f-person/git-blame.nvim",
-	event = "VeryLazy",
-	opts = {
-		enabled = true,
-		message_template = "  <summary> • <author> • <date> ",
-		date_format = "%r",
+	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		opts = {
+			enabled = true,
+			message_template = "  <summary> • <author> • <date> ",
+			date_format = "%r",
+			display_virtual_text = 0,
+		},
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = function(_, opts)
+			local git_blame = require("gitblame")
+
+			table.insert(opts.sections.lualine_x, 1, {
+				git_blame.get_current_blame_text,
+				cond = git_blame.is_blame_text_available,
+				draw_empty = false,
+			})
+		end,
 	},
 }
