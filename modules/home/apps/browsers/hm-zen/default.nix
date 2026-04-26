2592e25ba14045a137bbe30d144fc49798e9f144
jay <me@jayparry.dev>
2026-04-26 21:20:44 +1000

    zen: start configuring zen

--- modules/home/apps/browsers/hm-zen/default.nix
+++ modules/home/apps/browsers/hm-zen/default.nix
@@ -23,7 +23,7 @@ in
       setAsDefaultBrowser = true;
 
       policies = {
-        AutofillAddressEnabled = true;
+        AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
         DisableAppUpdate = true;
         DisableFeedbackCommands = true;
@@ -41,6 +41,43 @@ in
           EmailTracking = true;
           SuspectedFingerprinting = true;
         };
+        SanitizeOnShutdown = {
+          FormData = true;
+          Cache = true;
+        };
+      };
+
+      profiles.${config.home.username} = rec {
+        id = 0; # Profile IDs must be sequential starting from 0
+        settings = {
+          "zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url" = true;
+          "zen.tabs.vertical.right-side" = true;
+          "zen.view.compact.enable-at-startup" = true;
+          "zen.view.compact.hide-toolbar" = true;
+          "zen.view.compact.toolbar-flash-popup" = true;
+          "zen.view.use-single-toolbar" = false;
+          "zen.welcome-screen.seen" = true;
+          "zen.workspaces.separate-essentials" = false;
+        };
+        keyboardShortcutsVersion = 17; # pin to detect regressions
+
+        spacesForce = true;
+        spaces = {
+          "personal" = {
+            id = "260c5608-43a4-4c86-adfb-8aca1a04aa28";
+            icon = "🏡";
+            position = 1000;
+          };
+
+          "uni" = {
+            id = "4e91323f-cfa9-4976-a869-b450d4f04554";
+            icon = "🎓";
+            position = 2000;
+          };
+        };
+
+        pinsForce = true;
+        pins = { };
       };
     };
   };
