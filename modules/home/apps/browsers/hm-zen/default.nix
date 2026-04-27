252971c0aaf413ff7a7bd53b3be8a3ec5706c69d
jay <me@jayparry.dev>
2026-04-27 16:39:17 +1000

    zen: add more pins

--- modules/home/apps/browsers/hm-zen/default.nix
+++ modules/home/apps/browsers/hm-zen/default.nix
@@ -78,12 +78,65 @@ in
 
         pinsForce = true;
         pins = {
+          # NOTE: essential pins
           "codeberg" = {
             id = "740bea3c-861c-4e4e-8c25-e3989bf2c620";
             workspace = spaces.personal.id;
             url = "https://codeberg.org/berybin";
             isEssential = true;
             position = 0;
+          };
+          "github" = {
+            id = "526ab33d-df24-43a5-ad07-af636af4b0e6";
+            workspace = spaces.personal.id;
+            url = "https://github.com/berybin?tab=repositories";
+            isEssential = true;
+            position = 1;
+          };
+          "proton mail" = {
+            id = "";
+            workspace = spaces.personal.id;
+            url = "https://mail.proton.me/u/3/inbox";
+            isEssential = true;
+            position = 2;
+          };
+          "proton calendar" = {
+            id = "111464c7-4c53-4650-a50c-afd1c9e8daab";
+            workspace = spaces.personal.id;
+            url = "https://calendar.proton.me/u/3/";
+            isEssential = true;
+            position = 3;
+          };
+          "proton drive" = {
+            id = "d9803272-b4d3-4203-b7fb-c47239cdb2d4";
+            workspace = spaces.personal.id;
+            url = "https://drive.proton.me/u/3/";
+            isEssential = true;
+            position = 4;
+          };
+
+          # NOTE: regular pins
+          "youtube" = {
+            id = "0de4625b-4509-440f-831d-38cf815bf4f9";
+            workspace = spaces.personal.id;
+            url = "https://www.youtube.com/feed/subscriptions";
+            position = 5;
+          };
+
+          # NOTE: /nix pin group
+          "nix" = {
+            id = "75d80b05-c3f4-460d-8064-ea1bcc2ed05c";
+            workspace = spaces.personal.id;
+            editedTitle = true;
+            isGroup = true;
+            position = 100;
+          };
+          "nixos search" = {
+            id = "a0770f40-6f7d-4bda-8374-2cf08e23d51e";
+            workspace = spaces.personal.id;
+            url = "https://search.nixos.org/packages?channel=unstable&include_modular_service_options=1&include_nixos_options=1";
+            folderParentId = pins.nix.id;
+            position = 102;
           };
         };
       };
