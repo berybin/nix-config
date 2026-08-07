a24d6df07a4360f7ddc5bcfadc0ea3b5aabeaf8c
jay <me@jayparry.dev>
2026-08-07 21:53:20 +1000

    vscodium: configure extensions

--- modules/home/apps/vscodium/default.nix
+++ modules/home/apps/vscodium/default.nix
@@ -17,6 +17,9 @@ in
 
     home.packages = with pkgs; [
       ripgrep
+      python3
+      uv
+      # platformio-core
     ];
 
     programs.vscodium = {
@@ -26,6 +29,40 @@ in
         extensions = with pkgs.open-vsx; [
           astro-build.astro-vscode
           mads-hartmann.bash-ide-vscode
+          mk12.better-git-line-blame
+          pkgs.vscode-marketplace.ms-vscode.cpptools
+          matthewpi.caddyfile-support
+          ms-vscode.cmake-tools
+          naumovs.color-highlight
+          ms-azuretools.vscode-containers
+          mkhl.direnv
+          docker.docker
+          dracula-theme.theme-dracula
+          editorconfig.editorconfig
+          # microhobby.linuxkerneldev # Embedded Linux Kernel Dev - disabled for now
+          dbaeumer.vscode-eslint
+          tamasfe.even-better-toml
+          eliostruyf.vscode-front-matter
+          mhutchie.git-graph
+          golang.go
+          # ritwickdey.LiveServer
+          ms-vscode.makefile-tools
+          yzhang.markdown-all-in-one
+          davidanson.vscode-markdownlint
+          jnoortheen.nix-ide
+          pkgs.open-vsx."42crunch".vscode-openapi
+          oxc.oxc-vscode
+          pioarduino.pioarduino-ide # Fork of PlatformIO: https://github.com/pioarduino/pioarduino-vscode-ide/blob/HEAD/WHY_THIS_FORK.md
+          esbenp.prettier-vscode
+          yoavbls.pretty-ts-errors
+          mechatroner.rainbow-csv
+          renesaarsoo.sql-formatter-vsc
+          bradlc.vscode-tailwindcss
+          vscode-icons-team.vscode-icons
+          tomoki1207.pdf
+          vue.volar
+          redhat.vscode-xml
+          redhat.vscode-yaml
         ];
       };
     };
