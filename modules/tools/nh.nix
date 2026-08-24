ea5b27c32de2ca7136769d9f6a4ea27dfcd8fe4f
jay <me@jayparry.dev>
2026-08-24 12:55:00 +1000

    nh: fix the flake path option declaration

--- modules/tools/nh.nix
+++ modules/tools/nh.nix
@@ -7,14 +7,15 @@ let
   cfg = config.tools.nh;
 in
 {
-  flake.modules.nixos.nh = {
-    options.tools.nh.flakePath = lib.mkOption {
-      type = lib.types.str;
-      default = "/home/jay/nix-config";
-      description = "Absolute path to the flake directory used by nh.";
-    };
 
-    config.programs.nh = {
+  options.tools.nh.flakePath = lib.mkOption {
+    type = lib.types.str;
+    default = "/home/jay/nix-config";
+    description = "Absolute path to the flake directory used by nh.";
+  };
+
+  config.flake.modules.nixos.nh = {
+    programs.nh = {
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 14d --keep 4";
