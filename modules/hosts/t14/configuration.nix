6be8f609e3acd1ff86500db8741cd0fbe9ba1b31
jay <me@jayparry.dev>
2026-09-05 13:11:22 +1000

    t14: formatting changes

--- modules/hosts/t14/configuration.nix
+++ modules/hosts/t14/configuration.nix
@@ -8,23 +8,23 @@ let
 in
 {
   flake.modules.nixos.t14 = {
-    imports =
-      with self.modules.nixos;
-      [
-        system-desktop
-        desktop
-      ]
-      ++ [ (self.lib.loadNixosAndHmModulesForUser config modules "jay") ];
-
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.sharedModules = [
-      {
-        programs.niri.settings.outputs."eDP-1" = {
-          scale = 1;
-        };
-      }
+    imports = with self.modules.nixos; [
+      system-desktop
+      desktop
+      (self.lib.loadNixosAndHmModulesForUser config modules "jay")
     ];
+
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      sharedModules = [
+        {
+          programs.niri.settings.outputs."eDP-1" = {
+            scale = 1;
+          };
+        }
+      ];
+    };
     system.stateVersion = "26.05";
   };
 }
