72895fb425a26c7a1b97bbe767a8706aa00d997e
jay <me@jayparry.dev>
2026-09-06 18:53:26 +1000

    office: swap okular for papers

--- modules/apps/office.nix
+++ modules/apps/office.nix
@@ -2,8 +2,8 @@
   flake.modules.nixos.office = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       calibre
-      kdePackages.okular
       onlyoffice-desktopeditors
+      papers
     ];
   };
 }
