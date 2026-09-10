e91fdd1749315a37304e93fe80fd0d73ee351162
jay <me@jayparry.dev>
2026-09-10 21:11:37 +1000

    zen: only sync pinned tabs

--- modules/apps/zen/_settings.nix
+++ modules/apps/zen/_settings.nix
@@ -1,9 +1,10 @@
 {
 
   "zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url" = true;
-  "zen.view.compact.enable-at-startup" = true;
   "zen.tabs.vertical.right-side" = true;
-  "zen.welcome-screen.seen" = true;
+  "zen.view.compact.enable-at-startup" = true;
   "zen.view.use-single-toolbar" = false; # Sidebar and Top Toolbar
+  "zen.welcome-screen.seen" = true;
+  "zen.window-sync.sync-only-pinned-tabs" = true;
   "zen.workspaces.continue-where-left-off" = true;
 }
