24cca831e6e224b8572c03f07740fc41a08c79f0
jay <me@jayparry.dev>
2026-09-14 21:08:52 +1000

    music: add music related module

---
+++ modules/apps/music.nix
@@ -0,0 +1,7 @@
+{
+  flake.modules.homeManager.music = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      puddletag
+    ];
+  };
+}
--- modules/hosts/workstation/configuration.nix
+++ modules/hosts/workstation/configuration.nix
@@ -4,6 +4,7 @@ let
     "git"
     "gpg"
     "media"
+    "music"
     "nh"
     "office"
     "social"
