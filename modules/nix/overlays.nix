631833ba2189fa6b05dd14e77841f43f66144a18
jay <me@jayparry.dev>
2026-09-14 18:38:00 +1000

    overlays: pin freecad to stable

--- modules/nix/overlays.nix
+++ modules/nix/overlays.nix
@@ -1,8 +1,15 @@
 { inputs, ... }:
 {
-  flake.overlays = {
-    default = inputs.nixpkgs.lib.composeManyExtensions [
-      inputs.nur.overlays.default
-    ];
-  };
+  flake.overlays =
+    let
+      mkStablePkg = pkgToPin: self: super: {
+        ${pkgToPin} = inputs.nixpkgs-stable.legacyPackages.${self.stdenv.hostPlatform.system}.${pkgToPin};
+      };
+    in
+    {
+      default = inputs.nixpkgs.lib.composeManyExtensions [
+        inputs.nur.overlays.default
+        (mkStablePkg "freecad")
+      ];
+    };
 }
